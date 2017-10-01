2de3907526c6357a6e433505aa6558820ddf2e74
Christian Bernini <cgbas1987@gmail.com>
2017-10-01 20:03:44 -0300

    c3 completo

--- ex_c3.py
+++ ex_c3.py
@@ -9,4 +9,68 @@ def right_justify(s):
     print(justified)
 
 
-right_justify("Bulls on parade")
+#right_justify("Bulls on parade")
+
+
+# Exercício 3.2
+
+
+def do_twice(f, a):
+    f(a)
+    f(a)
+
+
+def print_spam():
+    print('spam')
+
+
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+
+def print_once(v):
+    print(v, end='')
+
+
+def do_four(f, v):
+    do_twice(f, v)
+    do_twice(f, v)
+
+
+def do_four_b(f):
+    f()
+    f()
+    f()
+    f()
+
+
+def print_four(v):
+    print(v, end='')
+    print(v, end='')
+    print(v, end='')
+    print(v, end='')
+
+
+# do_four('chris')
+# do_twice(print_twice, 'spam')
+
+
+# Exercício 3.3
+
+
+def desenhar_grade():
+    line1 = '+ - - - - '
+    line2 = '|         '
+    print_four(line1 * 3 + '+ - - - - +' + (('\n' + line2 * 4 + '|' + '\n') * 4))
+    print(line1 * 3 + '+ - - - - +')
+
+
+
+desenhar_grade()
+    
+
+
+
+
+
