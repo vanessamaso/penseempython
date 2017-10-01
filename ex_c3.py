ddc9e3b454b4d5aed3a21114d165a71979b8c8fc
Christian Bernini <cgbas1987@gmail.com>
2017-10-01 18:06:12 -0300

    updating function definitions

---
+++ ex_c3.py
@@ -0,0 +1,12 @@
+##!/usr/bin/python3
+
+def print_lyrics() :
+    #print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night, and work all day.")
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics()
+
